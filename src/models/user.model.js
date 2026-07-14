d8cf2245f292dd82fb2bac424a22af809ee3036f
Abhijeet Fulpagare <fulpagareabhijeet39@gmail.com>
2026-07-14 20:12:14 +0530

    User model Created

---
+++ src/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true,"Username is required"],
+        unique:[true,"Username must be unique"]
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: [true, "email must be unique"]
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+    }
+})
+
+const userModel = mongoose.model("user", UserSchema)
+
+export default userModel;
