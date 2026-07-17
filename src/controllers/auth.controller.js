6f7e50f1badc50e7cb0399e727530afde69fc311
Abhijeet Fulpagare <fulpagareabhijeet39@gmail.com>
2026-07-17 13:37:29 +0530

    Access token and refresh Token Function added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,19 +31,31 @@ const registerController = async (req, res) => {
             password: hash,
         });
 
-        const token = jwt.sign(
+        const accessToken = jwt.sign(
             { id: user._id },
             config.JWT_SECRET,
-            { expiresIn: "7d" }
+            { expiresIn: "15m" }
         );
 
+        const refreshToken = jwt.sign(
+            { id: user._id },
+            config.JWT_SECRET,
+            { expiresIn: "7d" } //7days
+        );
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 7 * 24 *60 * 60 *1000
+        })
         res.status(201).json({
             message: "User created successfully",
             user: {
                 username: user.username,
                 email: user.email,
             },
-            token,
+            accessToken,
         });
     } catch (err) {
         console.error(err);
@@ -92,4 +104,42 @@ const getMe = async (req, res) => {
     }
 }
 
-export { registerController , getMe };
+const RefreshToken = (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        
+        if (!refreshToken) {
+            return res.status(401).json({ message: "Token do not exists" });
+        }
+
+
+        const decode = jwt.verify(refreshToken, config.JWT_SECRET);
+
+        if (!decode) {
+            return res.status(401).json({ message: "Token is invalid" });
+        }
+
+        const newRefreshToken = jwt.sign({ id: decode.id }, config.JWT_SECRET, { expiresIn: "7d" });
+
+
+        const accessToken = jwt.sign({ id: decode.id }, config.JWT_SECRET,{expiresIn:"15m"});
+
+        res.cookie("refreshToken", newRefreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        res.status(200).json({ message: "Access token refreshed Successfully", accessToken });
+    }
+    catch (err)
+    {
+        console.error(err);
+
+        res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+}
+
+export { registerController, getMe, RefreshToken };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { registerController , getMe } from "../controllers/auth.controller.js";
+import { registerController, getMe, RefreshToken } from "../controllers/auth.controller.js";
 const authRouter = Router();
 
 authRouter.post("/register", registerController)
-authRouter.get("/get-me",getMe)
+authRouter.get("/get-me", getMe);
+authRouter.get("/refresh-token",RefreshToken)
+
 
 
 export default authRouter;
